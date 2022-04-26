01157e5060d857cfd6b39a4c1debbdba9e987ecf
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-04-26 15:29:26 -0500

    Add individual post route

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,9 @@ class PostsController < ApplicationController
   def index
     @user_id = params[:user_id]
   end
+
+  def show
+    @user_id = params[:user_id]
+    @post_id = params[:id]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
   get 'users/:user_id/posts', to: 'posts#index'
 
+  get 'users/:user_id/posts/:id', to: 'posts#show'
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
