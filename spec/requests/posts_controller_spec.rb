3d6cde2d5205d7554f92c2abffc0cce6236def27
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-04-27 16:08:14 -0500

    Add show test cases for posts

--- spec/requests/posts_controller_spec.rb
+++ spec/requests/posts_controller_spec.rb
@@ -28,4 +28,38 @@ RSpec.describe "PostsControllers", type: :request do
       expect(response.body).to include('Displays list of posts for user number: 2')
     end
   end
+
+  describe "GET /show" do
+    it 'has a success status' do 
+      get '/users/2/posts/5'
+      expect(response).to have_http_status(:ok)
+    end
+
+    context 'renders show template' do
+      it 'renders correct template' do 
+        get '/users/2/posts/5'
+        expect(response).to render_template(:show)
+      end
+
+      it "doesn't render other template" do 
+        get '/users/2/posts/5'
+        expect(response).to_not render_template(:index)
+      end
+    end
+
+    it 'assings user_id to local variable' do 
+      get '/users/2/posts/5'
+      expect(assigns(:user_id)).to eq('2')
+    end
+
+    it 'assings id to post_id' do 
+      get '/users/2/posts/5'
+      expect(assigns(:post_id)).to eq('5')
+    end
+
+    it 'contains show placeholder' do 
+      get '/users/2/posts/5'
+      expect(response.body).to include('Shows post number: 5 of user number: 2')
+    end
+  end
 end
