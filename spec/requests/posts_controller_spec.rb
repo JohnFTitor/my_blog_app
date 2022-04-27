14a61551e65d8fd05c853fcd575e3ef8d63aa2f4
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-04-27 16:13:18 -0500

    Fix linters

--- spec/requests/posts_controller_spec.rb
+++ spec/requests/posts_controller_spec.rb
@@ -1,63 +1,63 @@
 require 'rails_helper'
 
-RSpec.describe "PostsControllers", type: :request do
-  describe "GET /index" do
-    it 'has a success status' do 
+RSpec.describe 'PostsControllers', type: :request do
+  describe 'GET /index' do
+    it 'has a success status' do
       get '/users/2/posts'
       expect(response).to have_http_status(:ok)
     end
 
     context 'renders index template' do
-      it 'renders correct template' do 
+      it 'renders correct template' do
         get '/users/2/posts'
         expect(response).to render_template(:index)
       end
-      it "doesn't render other template" do 
+      it "doesn't render other template" do
         get '/users/2/posts'
         expect(response).to_not render_template(:show)
       end
     end
 
-    it 'assings user_id to local variable' do 
-      get "/users/2/posts"
+    it 'assings user_id to local variable' do
+      get '/users/2/posts'
       expect(assigns(:user_id)).to eq('2')
-    end  
+    end
 
-    it 'has index placeholder' do 
+    it 'has index placeholder' do
       get '/users/2/posts'
       expect(response.body).to include('Displays list of posts for user number: 2')
     end
   end
 
-  describe "GET /show" do
-    it 'has a success status' do 
+  describe 'GET /show' do
+    it 'has a success status' do
       get '/users/2/posts/5'
       expect(response).to have_http_status(:ok)
     end
 
     context 'renders show template' do
-      it 'renders correct template' do 
+      it 'renders correct template' do
         get '/users/2/posts/5'
         expect(response).to render_template(:show)
       end
 
-      it "doesn't render other template" do 
+      it "doesn't render other template" do
         get '/users/2/posts/5'
         expect(response).to_not render_template(:index)
       end
     end
 
-    it 'assings user_id to local variable' do 
+    it 'assings user_id to local variable' do
       get '/users/2/posts/5'
       expect(assigns(:user_id)).to eq('2')
     end
 
-    it 'assings id to post_id' do 
+    it 'assings id to post_id' do
       get '/users/2/posts/5'
       expect(assigns(:post_id)).to eq('5')
     end
 
-    it 'contains show placeholder' do 
+    it 'contains show placeholder' do
       get '/users/2/posts/5'
       expect(response.body).to include('Shows post number: 5 of user number: 2')
     end
--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -1,59 +1,59 @@
 require 'rails_helper'
 
-RSpec.describe "UsersControllers", type: :request do
-  describe "GET /index" do
-    it 'is the root of the website' do 
+RSpec.describe 'UsersControllers', type: :request do
+  describe 'GET /index' do
+    it 'is the root of the website' do
       get '/'
       expect(response).to have_http_status(:ok)
       expect(response).to render_template(:index)
     end
 
-    it 'has a success status' do 
+    it 'has a success status' do
       get '/users'
       expect(response).to have_http_status(:ok)
     end
 
     context 'renders index template' do
-      it 'renders correct template' do 
+      it 'renders correct template' do
         get '/users'
         expect(response).to render_template(:index)
       end
-      it "doesn't render other template" do 
+      it "doesn't render other template" do
         get '/users'
         expect(response).to_not render_template(:show)
       end
     end
 
-    it 'has index placeholder' do 
+    it 'has index placeholder' do
       get '/users'
       expect(response.body).to include('List all users')
     end
   end
 
-  describe "GET /show" do
-    it 'has a success status' do 
+  describe 'GET /show' do
+    it 'has a success status' do
       get '/users/2'
       expect(response).to have_http_status(:ok)
     end
 
     context 'renders show template' do
-      it 'renders correct template' do 
+      it 'renders correct template' do
         get '/users/2'
         expect(response).to render_template(:show)
       end
 
-      it "doesn't render other template" do 
+      it "doesn't render other template" do
         get '/users/2'
         expect(response).to_not render_template(:index)
       end
     end
 
-    it 'assings id to user' do 
-      get "/users/2"
+    it 'assings id to user' do
+      get '/users/2'
       expect(assigns(:user)).to eq('2')
     end
 
-    it 'contains show placeholder' do 
+    it 'contains show placeholder' do
       get '/users/2'
       expect(response.body).to include('Shows information for user number: 2')
     end
