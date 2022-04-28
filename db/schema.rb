ef92ba3a42d48df87e56beb34cd121cfb94bca99
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-04-28 15:45:42 -0500

    Create comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_28_192231) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_28_204452) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "author_id"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_comments_on_author_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -23,4 +31,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_28_192231) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "users", column: "author_id"
 end
