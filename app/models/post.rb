d2c5701581d564934d56612b98cd6c5570084797
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-05-02 16:37:27 -0500

    Add Post methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,11 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: "User"
   has_many :likes
   has_many :comments
+
+  def self.update_count(user)
+    count = user.posts.count
+    user.update(posts_counter: count)
+  end
+
+  scope :recent_comments, -> (post) { post.comments.order(created_at: :desc).limit(5) }
 end
