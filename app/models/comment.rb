c4fcd8a57b8cbf1c8c040ded46882c8a43ab6663
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-05-02 15:07:26 -0500

    Fix belongs to relationship

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: "User"
   belongs_to :post 
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,4 @@
 class Like < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: "User"
   belongs_to :post
 end
