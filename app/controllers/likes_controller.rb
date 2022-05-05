db84fc12f8d97c4e0517f74f62b79f83169c1a36
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-05-04 21:29:08 -0500

    Add like functionality

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,4 +2,24 @@ class LikesController < ApplicationController
   def index
     redirect_to users_path
   end
+
+  def create
+    like = Like.new(get_params)
+    like.author = current_user
+    respond_to do |format|
+      format.html do 
+        if like.save 
+          redirect_back(fallback_location: root_path)
+        end
+      end
+    end
+  end
+
+  private 
+
+  def get_params
+    response = params.require(:like).permit(:post)
+    response[:post] = Post.find(response[:post])
+    response
+  end
 end
