25ff43eef852ba0ed8cdba186b9b92f08f7c3e81
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-05-05 21:18:58 -0500

    Define failing test cases for post

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -28,6 +28,41 @@ RSpec.describe 'Post', type: :model do
       expect(recent_comments.length).to eq(5)
       expect(recent_comments).to eq(last_five_comments.reverse)  
     end
+
+    it 'should increase post_counter method of author' do 
+      expect(@user.posts_counter).to eq(1)
+    end
   end
 
+  describe 'should implement validations' do 
+    it 'should not have a blank title' do 
+      subject.title = ''
+      expect(subject).to_not be_valid
+    end
+
+    it 'should not be greater than 250 char' do 
+      subject.title = 'z' * 300
+      expect(subject).to_not be_valid
+    end
+
+    it 'should have comments_counter' do 
+      subject.comments_counter = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'should have likes_counter' do 
+      subject.likes_counter = nil
+      expect(subject).to_not be_valid 
+    end
+
+    it 'where comments are greater than or equal to zero' do 
+      subject.comments_counter = -1
+      expect(subject).to_not be_valid
+    end
+
+    it 'where likes are greater than or equal to zero' do 
+      subject.likes_counter = -1
+      expect(subject).to_not be_valid
+    end
+  end
 end
