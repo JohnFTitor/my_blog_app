637ad25ff9f6b9910d88e681e9e2737ae72ce8e0
fherrerao <fherrera0206@gmail.com>
2022-05-10 11:17:49 -0500

    Add sign_up fields in view and controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::Base  
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters,if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email password name photo posts_counter])
+  end
+
 end
