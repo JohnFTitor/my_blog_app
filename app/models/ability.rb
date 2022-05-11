d6e1100b80093d1290b118da3ae9da5381c638f6
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-05-11 15:27:36 -0500

    Add functionality to accept a guest user
    - Navbar with Log in Button

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,9 +2,14 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    can :read, :all
+
+    return unless user
+
     can :manage, Post, author_id: user.id
     can :manage, Comment, author_id: user.id
-    can :read, :all
+    can :manage, Like, author_id: user.id
+
     return unless user.admin?
 
     can :delete, Post
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-  has_many :comments, foreign_key: 'author_id'
-  has_many :posts, foreign_key: 'author_id'
-  has_many :likes, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id', dependent: :delete_all
+  has_many :posts, foreign_key: 'author_id', dependent: :delete_all
+  has_many :likes, foreign_key: 'author_id', dependent: :delete_all
   validates :name, presence: true
   validates :posts_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
