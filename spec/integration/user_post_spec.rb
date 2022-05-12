00e321e98c95e8864a74909026dae9ceae456902
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-05-12 12:22:34 -0500

    Finish post#index test cases

--- spec/integration/user_post_spec.rb
+++ spec/integration/user_post_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'User Show Page', type: :feature do
       User.destroy_all
       @user= User.create(id: 1, name: 'User-1', photo: 'user_image-1', bio: 'some1', email: "user1@example.com", password: "123456", confirmed_at: DateTime.now, posts_counter: 0)      
       (1..10).each do |i|
-        post = Post.new(id: i, title: "title-#{i}", text: 'text', comments_counter: 0, likes_counter: 0)
+        post = Post.new(id: i, title: "title-#{i}", text: 'a'*300, comments_counter: 0, likes_counter: 0)
         post.author = @user
         post.save
         (1..10).each do |j|
@@ -43,7 +43,8 @@ RSpec.describe 'User Show Page', type: :feature do
     end
 
     it "should have the post's body" do
-      expect(page).to have_content('text')
+      post = page.find('a', id: 'Post: 5')      
+      expect(post).to have_content('a'*147)
     end
 
     it "should have the first comments on a post" do
@@ -57,6 +58,18 @@ RSpec.describe 'User Show Page', type: :feature do
       post = page.find('a', id: 'Post: 5')      
       expect(post).to have_content('Comments: 10')
     end
-    
+
+    it 'should have how many comments a post has' do
+      post = page.find('a', id: 'Post: 5')      
+      expect(post).to have_content('Likes: 0')
+    end
+
+    it "should redirect me to that post's show page" do
+      post = page.find('a', id: 'Post: 3')
+
+      post.click
+
+      expect(page).to have_current_path(user_post_path(id: 3, user_id: 1))
+    end
   end    
 end
