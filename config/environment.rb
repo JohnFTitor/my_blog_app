10529d30509a5b8c58a225bdea3a9550bf5adc30
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-07-11 15:19:05 -0500

    Add sendgrid configuration

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  user_name: 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
+  password: Rails.application.credentials.dig(:sendgrid, :api_key), # This is the secret sendgrid API key which was issued during API key creation
+  domain: 'heroku.com',
+  address: 'smtp.sendgrid.net',
+  port: 587,
+  authentication: :plain,
+  enable_starttls_auto: true
+}
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,11 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # Configure Sendgrid
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'jf-blog-app.herokuapp.com', :protocol => 'https' }
+
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
